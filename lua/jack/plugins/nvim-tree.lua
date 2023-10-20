eaf99677e9b9f58314bbc187ad052f171962aa29
Giacomo Pasin <giacomo.pasin@gmail.com>
2023-10-20 08:27:36 +0200

    Commented nvim-tree

--- lua/jack/plugins/nvim-tree.lua
+++ lua/jack/plugins/nvim-tree.lua
@@ -1,58 +1,59 @@
-return {
-    "nvim-tree/nvim-tree.lua",
-    config = function ()
-         local nvimtree = require("nvim-tree")
-
-    -- recommended settings from nvim-tree documentation
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-
-    -- change color for arrows in tree to light blue
-    vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
-    vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
-
-    -- configure nvim-tree
-    nvimtree.setup({
-      view = {
-        width = 35,
-      },
-      -- change folder arrow icons
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = ">", -- arrow when folder is closed
-              arrow_open = "v", -- arrow when folder is open
-            },
-          },
-        },
-      },
-      -- disable window_picker for
-      -- explorer to work well with
-      -- window splits
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-      filters = {
-        custom = { ".DS_Store" },
-      },
-      git = {
-        ignore = false,
-      },
-    })
-
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    keymap.set("n", "<leader>eR", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-    end,
-}
+return {}
+-- return {
+--     "nvim-tree/nvim-tree.lua",
+--     config = function ()
+--          local nvimtree = require("nvim-tree")
+--
+--     -- recommended settings from nvim-tree documentation
+--     vim.g.loaded_netrw = 1
+--     vim.g.loaded_netrwPlugin = 1
+--
+--     -- change color for arrows in tree to light blue
+--     vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
+--     vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
+--
+--     -- configure nvim-tree
+--     nvimtree.setup({
+--       view = {
+--         width = 35,
+--       },
+--       -- change folder arrow icons
+--       renderer = {
+--         indent_markers = {
+--           enable = true,
+--         },
+--         icons = {
+--           glyphs = {
+--             folder = {
+--               arrow_closed = ">", -- arrow when folder is closed
+--               arrow_open = "v", -- arrow when folder is open
+--             },
+--           },
+--         },
+--       },
+--       -- disable window_picker for
+--       -- explorer to work well with
+--       -- window splits
+--       actions = {
+--         open_file = {
+--           window_picker = {
+--             enable = false,
+--           },
+--         },
+--       },
+--       filters = {
+--         custom = { ".DS_Store" },
+--       },
+--       git = {
+--         ignore = false,
+--       },
+--     })
+--
+--     -- set keymaps
+--     local keymap = vim.keymap -- for conciseness
+--
+--     keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+--     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+--     keymap.set("n", "<leader>eR", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+--     end,
+-- }
