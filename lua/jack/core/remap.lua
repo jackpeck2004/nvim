b4f87de58d8cf55d5c9a9b7648edb14c8e6ed3b0
Giacomo Pasin <giacomo.pasin@gmail.com>
2024-06-04 09:24:32 +0200

    Added remap for copying to clipboard

--- lua/jack/core/remap.lua
+++ lua/jack/core/remap.lua
@@ -7,3 +7,5 @@ vim.keymap.set("n", "<leader>tt", vim.cmd.tabnew)
 vim.keymap.set("n", "<leader>tn", vim.cmd.tabnext)
 vim.keymap.set("n", "<leader>tp", vim.cmd.tabprev)
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
