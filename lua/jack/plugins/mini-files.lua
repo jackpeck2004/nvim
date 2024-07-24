aac17fc94c01018be4b4b57bb3df7feb226c71cf
Giacomo Pasin <giacomo.pasin@gmail.com>
2024-07-24 15:14:25 +0200

    Made mini files open in current directory

--- lua/jack/plugins/mini-files.lua
+++ lua/jack/plugins/mini-files.lua
@@ -13,6 +13,10 @@ return {
 
         local opts = { noremap = true, silent = true }
         opts.desc = "Open file explorer"
-        keymap.set('n', '<leader>pv', MiniFiles.open, opts)
+        keymap.set('n', '<leader>pv', function ()
+            local bufname = vim.api.nvim_buf_get_name(0)
+            local dir = vim.fn.fnamemodify(bufname, ":h")
+            MiniFiles.open(dir)
+        end, opts)
     end
 }
