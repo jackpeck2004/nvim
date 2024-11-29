977f50579fdd8428b5d72ae32b7ded5855cb3426
Giacomo Pasin <giacomo.pasin@gmail.com>
2024-11-29 17:59:51 +0100

    added some remaps

--- lua/jack/plugins/remote-sshfs.lua
+++ lua/jack/plugins/remote-sshfs.lua
@@ -2,7 +2,14 @@ return {
  "nosduco/remote-sshfs.nvim",
  dependencies = { "nvim-telescope/telescope.nvim" },
  opts = {
-  -- Refer to the configuration section below
-  -- or leave empty for defaults
+     connections = {
+       vim.fn.expand "$HOME" .. "/.ssh/config",
+     }
  },
+ config = function()
+     local api = require('remote-sshfs.api')
+     vim.keymap.set('n', '<leader>rc', api.connect, {})
+     vim.keymap.set('n', '<leader>rd', api.disconnect, {})
+     vim.keymap.set('n', '<leader>re', api.edit, {})
+ end
 }
